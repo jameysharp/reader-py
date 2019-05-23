7a42c274f59154b7a9524ea4aa7c85fdd7af4d40
Jamey Sharp <jamey@minilop.net>
2019-05-22 18:31:38 -0700

    Extract full history from WordPress feeds too

--- feeds.py
+++ feeds.py
@@ -1,8 +1,10 @@
 import feedparser
 import io
+import itertools
 import scrapy
 import time
 from twisted.internet import defer
+from urllib.parse import urlsplit, urlunsplit, parse_qsl, urlencode
 
 
 class FeedError(Exception):
@@ -36,6 +38,10 @@ def extract_feed(response):
     for link in doc.feed.get("links", ()):
         links[link["rel"]] = link["href"]
 
+    generator = doc.feed.get("generator_detail", ())
+    if generator:
+        generator = tuple(generator.values())
+
     entries = [
         {
             "title": e.get("title", ""),
@@ -56,6 +62,7 @@ def extract_feed(response):
         "archive": is_archive,
         "complete": is_complete,
         "links": links,
+        "generator": generator,
         "entries": entries,
     }
 
@@ -88,6 +95,8 @@ def full_history(crawler, url):
         result = base
     elif "prev-archive" in base["links"]:
         result = yield from_rfc5005(crawler, base, url)
+    elif wordpress_generated(response.headers.getlist("Link"), base["generator"]):
+        result = yield from_wordpress(crawler, url)
     else:
         raise FeedError("document {!r} is not complete but doesn't link to archives".format(url))
 
@@ -130,5 +139,59 @@ def from_rfc5005(crawler, base, url):
                 seen.add(entry["id"])
             else:
                 print("discarding duplicate entry {!r}".format(entry["id"]))
+
+    defer.returnValue(combined)
+
+
+def query_string_replace(url, **kwargs):
+    parts = urlsplit(url)
+    query = [(k, v) for k, v in parse_qsl(parts.query) if k not in kwargs]
+    query.extend(sorted(kwargs.items()))
+    return urlunsplit(parts._replace(query=urlencode(query)))
+
+
+def wordpress_generated(links, generator):
+    for link in links:
+        if b"api.w.org" in link:
+            return True
+    for ident in generator:
+        ident = ident.lower()
+        if "wordpress.com" in ident or "wordpress.org" in ident:
+            return True
+    return False
+
+
+@defer.inlineCallbacks
+def from_wordpress(crawler, url):
+    url = query_string_replace(
+        url,
+        feed="atom",
+        order="ASC",
+        orderby="modified",
+        paged=1,
+    )
+
+    response = yield crawler.enqueue_request(scrapy.Request(url))
+    combined = extract_feed(response)
+    url = combined["links"].get("self") or response.headers.get("Content-Location") or response.url
+
+    for page in itertools.count(2):
+        next_url = query_string_replace(url, paged=page)
+
+        response = yield crawler.enqueue_request(scrapy.Request(
+            next_url,
+            meta={
+                "handle_httpstatus_list": [404],
+            },
+            headers={
+                "Referer": url,
+            },
+        ))
+        if response.status == 404:
+            break
+
+        url = next_url
+        feed = extract_feed(response)
+        combined["entries"].extend(feed["entries"])
 
     defer.returnValue(combined)
